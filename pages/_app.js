ac1e64f1238f111e38cf2941da5476a69505bbf8
Jared Fischer <jaredfischer@MacBook-Pro-3.local>
2022-05-08 15:45:26 +0300

    cors fix attempt 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import "../styles/globals.css";
-import { Provider } from "react-redux";
-import { store } from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Provider store={store}>
-				<Component {...pageProps} />
-			</Provider>
+			<Component {...pageProps} />
 		</>
 	);
 }
